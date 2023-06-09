b285bca4bf23f1e6af1caf1e80face80f5257ffd
alexeira <alex.pincheira06@gmail.com>
2023-06-08 20:54:39 -0400

    create tw-card & separate user into a component

--- projects/01-reviewing-use-state/src/App.jsx
+++ projects/01-reviewing-use-state/src/App.jsx
@@ -1,3 +1,16 @@
+import './App.css'
+import TwitterUser from './components/TwitterUser'
+
 export default function App () {
-  return <main><h1>Hola</h1></main>
+  return (
+    <main>
+      <article className='tw-follow-card'>
+        <h4 className='tw-u-might'>Tal vez te guste</h4>
+        <TwitterUser userName='goncy' name='Goncy.tsx' />
+        <TwitterUser userName='midudev' name={'Miguel Ángel Durán'.split(' ', 2).join(' ') + '...'} />
+        <TwitterUser userName='alexeira' name='Alex Pincheira' />
+        <a>Mostrar más</a>
+      </article>
+    </main>
+  )
 }
