ac36a97babc42ebc6689fc771008e5cae3d326f5
alexeira <alex.pincheira06@gmail.com>
2023-06-09 02:23:38 -0400

    finished first project

--- projects/01-reviewing-use-state/src/App.jsx
+++ projects/01-reviewing-use-state/src/App.jsx
@@ -1,14 +1,41 @@
 import './App.css'
 import TwitterUser from './components/TwitterUser'
 
+const users = [
+  {
+    userName: 'goncy',
+    name: 'Goncy.tsx',
+    isVerified: true
+  },
+  {
+    userName: 'midudev',
+    name: 'Miguel Ángel Durán',
+    isVerified: true
+  },
+  {
+    userName: 'alexeira',
+    name: 'Alex Pincheira'
+  }
+]
+
 export default function App () {
   return (
     <main>
       <article className='tw-follow-card'>
         <h4 className='tw-u-might'>Tal vez te guste</h4>
-        <TwitterUser userName='goncy' name='Goncy.tsx' />
-        <TwitterUser userName='midudev' name={'Miguel Ángel Durán'.split(' ', 2).join(' ') + '...'} />
-        <TwitterUser userName='alexeira' name='Alex Pincheira' />
+        {
+          users.map(user => {
+            const { userName, name, isVerified } = user
+            return (
+              <TwitterUser
+                key={userName}
+                userName={userName}
+                name={name}
+                isVerified={isVerified}
+              />
+            )
+          })
+        }
         <a>Mostrar más</a>
       </article>
     </main>
--- projects/01-reviewing-use-state/src/components/TwitterUser.jsx
+++ projects/01-reviewing-use-state/src/components/TwitterUser.jsx
@@ -1,19 +1,41 @@
-export default function TwitterUser ({ userName, name }) {
-  return (
-    <>
-      <section>
-        <header>
-          <img alt='user-icon' src={`https://unavatar.io/${userName}`} />
-          <div>
-            <strong>{name}</strong>
-            <span>@{userName}</span>
-          </div>
-        </header>
+import { useState } from 'react'
+import Verified from './Verified'
 
-        <aside>
-          <button>Seguir</button>
-        </aside>
-      </section>
-    </>
+const MAX_LENGTH_NAME = 13
+
+export default function TwitterUser ({ userName, name, isVerified }) {
+  const [isFollowing, setIsFollowing] = useState(false)
+
+  const followText = isFollowing ? 'Siguiendo' : 'Seguir'
+  const buttonClassName = isFollowing ? 'tw-followed-btn' : 'tw-follow-btn'
+
+  const truncateName = (name) => {
+    if (name.length <= MAX_LENGTH_NAME) return name
+    return `${name.substring(0, MAX_LENGTH_NAME - 1)}...`
+  }
+
+  return (
+    <section>
+      <header>
+        <img alt='user-icon' src={`https://unavatar.io/${userName}`} />
+        <div>
+          <strong>
+            {truncateName(name)}
+            {isVerified && <Verified />}
+          </strong>
+          <span>@{userName}</span>
+        </div>
+      </header>
+
+      <aside>
+        <button
+          onClick={() => setIsFollowing(!isFollowing)}
+          className={buttonClassName}
+        >
+          <span className='tw-followed-text'>{followText}</span>
+          <span className='tw-unfollow-text'>Dejar de seguir</span>
+        </button>
+      </aside>
+    </section>
   )
 }
