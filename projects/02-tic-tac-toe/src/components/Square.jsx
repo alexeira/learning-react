bbe2edd847f6ed388f31479a246d940056be7dd7
alexeira <alex.pincheira06@gmail.com>
2023-06-11 00:20:38 -0400

    added prop borders

--- projects/02-tic-tac-toe/src/components/Square.jsx
+++ projects/02-tic-tac-toe/src/components/Square.jsx
@@ -1,10 +1,10 @@
-export default function Square ({ children, isSelected, updateBoard, index }) {
+export default function Square ({ children, isSelected, updateBoard, index, borders }) {
   function handleClick () {
     updateBoard(index)
   }
 
   return (
-    <div onClick={handleClick} className={`square ${isSelected ? 'is-selected' : ''}`}>
+    <div onClick={handleClick} className={`${borders} square ${isSelected ? 'is-selected' : ''}`}>
       {children}
     </div>
   )
