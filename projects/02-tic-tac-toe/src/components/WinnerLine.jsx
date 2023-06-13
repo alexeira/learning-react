8fea8b2f5452758f741c7e51d99b68d39609bf40
alexeira <alex.pincheira06@gmail.com>
2023-06-13 01:34:37 -0400

    created const for lines

--- projects/02-tic-tac-toe/src/components/WinnerLine.jsx
+++ projects/02-tic-tac-toe/src/components/WinnerLine.jsx
@@ -1,7 +1,19 @@
+import { WINNER_COMBOS } from '../constants'
+import Diagonal from './lines/Diagonal'
+import Horizontal from './lines/Horizontal'
+import Vertical from './lines/Vertical'
+
 export default function WinnerLine ({ winnerCombo }) {
+  const horizontal = WINNER_COMBOS.slice(0, 3)
+  const vertical = WINNER_COMBOS.slice(3, 6)
+  const diagonal = WINNER_COMBOS.slice(6, 7)
+  const invertedDiagonal = WINNER_COMBOS.slice(7, 8)
+
+  if (!winnerCombo) return
+
   return (
-    <section style={{ border: '1px solid red' }}>
-      <h1>Gano: {winnerCombo}</h1>
+    <section className='winner-line'>
+      {winnerCombo && <Vertical />}
     </section>
   )
 }
