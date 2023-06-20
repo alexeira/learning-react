9a3d36fd1c9e5f08bcfc12e7432b878e1453f5c0
alexeira <alex.pincheira06@gmail.com>
2023-06-19 22:04:37 -0400

    init 5th project

---
+++ projects/05-movie-finder/src/App.jsx
@@ -0,0 +1,16 @@
+export default function App () {
+  return (
+    <div>
+      <h1>Movie finder</h1>
+      <header>
+        <form>
+          <input placeholder='Avengers, Star Wars, Spider-Man...' />
+          <button type='submit'>Buscar</button>
+        </form>
+      </header>
+      <main>
+        <p>Resultados...</p>
+      </main>
+    </div>
+  )
+}
