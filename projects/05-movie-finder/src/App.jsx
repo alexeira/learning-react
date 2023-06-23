5a89532758de58074f7e106e8045a1bef67b5c87
alexeira <alex.pincheira06@gmail.com>
2023-06-22 23:37:01 -0400

    first version of movies

--- projects/05-movie-finder/src/App.jsx
+++ projects/05-movie-finder/src/App.jsx
@@ -1,15 +1,44 @@
+import Movies from './components/Movies'
+import useMovies from './hooks/useMovies'
+import useSearch from './hooks/useSearch'
+
 export default function App () {
+  const { movies } = useMovies()
+  const { search, updateSearch, error } = useSearch()
+
+  function handleSubmit (event) {
+    event.preventDefault()
+  }
+
+  function handleChange (event) {
+    const newSearch = event.target.value
+    if (newSearch.startsWith(' ')) return
+    updateSearch(newSearch)
+  }
+
   return (
     <div>
       <h1>Movie finder</h1>
       <header>
-        <form>
-          <input placeholder='Avengers, Star Wars, Spider-Man...' />
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={handleChange}
+            value={search}
+            name='search'
+            type='search'
+            placeholder='Avengers, Star Wars, Spider-Man...'
+            style={{
+              outline: 'none',
+              border: '2px solid black',
+              borderColor: error ? 'red' : 'black'
+            }}
+          />
           <button type='submit'>Buscar</button>
         </form>
+        {error && <small style={{ color: 'red' }}>{error}</small>}
       </header>
       <main>
-        <p>Resultados...</p>
+        <Movies movies={movies} />
       </main>
     </div>
   )
