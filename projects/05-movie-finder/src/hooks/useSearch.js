9cbd4cb39e9b8511f9bc6385eb4befa5bdafae0a
alexeira <alex.pincheira06@gmail.com>
2023-06-22 23:36:04 -0400

    created custom hooks for movies

---
+++ projects/05-movie-finder/src/hooks/useSearch.js
@@ -0,0 +1,33 @@
+import { useRef, useEffect, useState } from 'react'
+
+export default function useSearch () {
+  const [search, updateSearch] = useState('')
+  const [error, setError] = useState(null)
+  const isFirstInput = useRef(true)
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === ''
+      return
+    }
+
+    if (search === '') {
+      setError('No se puede buscar una pelicula vacia')
+      return
+    }
+
+    if (search.match(/^\d+$/)) {
+      setError('No se puede buscar una pelicula por numero')
+      return
+    }
+
+    if (search.length < 3) {
+      setError('La busqueda debe tener al menos 3 caracteres')
+      return
+    }
+
+    setError(null)
+  }, [search])
+
+  return { search, updateSearch, error }
+}
