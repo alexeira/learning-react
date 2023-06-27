f769ab822e0db1ce64947d2316dc5b6abad03144
alexeira <alex.pincheira06@gmail.com>
2023-06-26 23:37:05 -0400

    created custom hook useEllipsisTooltip for title attr on h3 that has ellipsis

--- projects/05-movie-finder/src/hooks/useEllipsisTooltip.js
+++ projects/05-movie-finder/src/hooks/useEllipsisTooltip.js
@@ -1,15 +1,15 @@
-import { useEffect, useState, useRef } from 'react'
+import { useState } from 'react'
 
 export default function useEllipsisTooltip () {
   const [tooltip, setTooltip] = useState(false)
-  const ref = useRef(null)
 
-  useEffect(() => {
-    console.log(tooltip, ref)
-    const element = ref.current
-    if (element && element.scrollWidth > element.clientWidth) return setTooltip(true)
+  function checkEllipsis (event) {
+    const element = event.target
+    if (element && element.scrollWidth > element.clientWidth) {
+      return setTooltip(true)
+    }
     return setTooltip(false)
-  }, [tooltip])
+  }
 
-  return { ref, tooltip, setTooltip }
+  return { tooltip, checkEllipsis }
 }
