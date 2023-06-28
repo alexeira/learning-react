1e03e63b3a680d32c5791de56b2796b18006637a
alexeira <alex.pincheira06@gmail.com>
2023-06-27 23:58:48 -0400

    added more conditions to custom hook

--- projects/05-movie-finder/src/hooks/useMovies.js
+++ projects/05-movie-finder/src/hooks/useMovies.js
@@ -1,13 +1,29 @@
+import { useRef, useState } from 'react'
 import { searchMovies } from '../services/movies'
-import { useState } from 'react'
 
 export default function useMovies ({ search }) {
   const [movies, setMovies] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const previousSearch = useRef(search)
 
   async function getMovies () {
-    const newMovies = await searchMovies({ search })
-    await setMovies(newMovies)
+    if (search === previousSearch.current) return
+
+    try {
+      setLoading(true)
+      setError(null)
+      console.log(search, previousSearch)
+      console.log(previousSearch, search)
+      previousSearch.current = search
+      const newMovies = await searchMovies({ search })
+      await setMovies(newMovies)
+    } catch (e) {
+      setError(e.message)
+    } finally {
+      setLoading(false)
+    }
   }
 
-  return { movies, getMovies }
+  return { movies, loading, getMovies }
 }
