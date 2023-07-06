9a7951293e8befa605b37baac122edc67ecd0716
alexeira <alex.pincheira06@gmail.com>
2023-07-06 14:38:58 -0400

    involved the App in the fiter context

--- projects/06-store/src/main.jsx
+++ projects/06-store/src/main.jsx
@@ -1,5 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { FiltersProviders } from './context/filters'
 
-createRoot(document.getElementById('root')).render(<App />)
+createRoot(document.getElementById('root')).render(
+  <FiltersProviders>
+    <App />
+  </FiltersProviders>
+)
