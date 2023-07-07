e8a894d713d95dad35798625ffd96d4020c8909b
alexeira <alex.pincheira06@gmail.com>
2023-07-06 20:03:15 -0400

    added cart for debug data of cart

--- projects/06-store/src/components/Footer.jsx
+++ projects/06-store/src/components/Footer.jsx
@@ -1,13 +1,16 @@
 import { useFilters } from '../hooks/useFilters'
 import './Footer.css'
 import { IS_DEVELOPMENT } from '../utils/config'
+import { useCart } from '../hooks/useCart'
 
 export function Footer () {
   const { filters } = useFilters()
+  const { cart } = useCart()
 
   return (
     <footer className='footer'>
       {IS_DEVELOPMENT && JSON.stringify(filters, null, 1)}
+      {IS_DEVELOPMENT && JSON.stringify(cart, null, 2)}
     </footer>
   )
 }
