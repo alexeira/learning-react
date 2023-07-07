dd6db42f699d218ff8c0228a14634975dac6a892
alexeira <alex.pincheira06@gmail.com>
2023-07-06 20:04:10 -0400

    envolved App content on CartProvider & added Cart component

--- projects/06-store/src/App.jsx
+++ projects/06-store/src/App.jsx
@@ -5,16 +5,19 @@ import { products as initialProducts } from './mocks/products.json'
 import { Products } from './components/Products'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
+import { Cart } from './components/Cart'
+import { CartProvider } from './context/cart'
 
 export default function App () {
   const { filterProducts } = useFilters()
   const filteredProducts = filterProducts({ initialProducts })
 
   return (
-    <>
+    <CartProvider>
       <Header />
+      <Cart />
       <Products products={filteredProducts} />
       <Footer />
-    </>
+    </CartProvider>
   )
 }
