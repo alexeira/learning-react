c65b95f8260d1dd27b5d0509ac0f17c61c804890
alexeira <alex.pincheira06@gmail.com>
2023-07-08 13:10:42 -0400

    using useReducer w/ object

--- projects/06-store/src/reducers/cart.js
+++ projects/06-store/src/reducers/cart.js
@@ -4,40 +4,46 @@ const updateLocalStorage = state => {
   window.localStorage.setItem('cart', JSON.stringify(state))
 }
 
-export const reducer = (state, action) => {
-  const { type: actionType, payload: actionPayload } = action
+const actions = {
+  ADD_TO_CART: (state, actionPayload) => {
+    const { id } = actionPayload
+    const productInCartIndex = state.findIndex(item => item.id === id)
 
-  switch (actionType) {
-    case 'ADD_TO_CART': {
-      const { id } = actionPayload
-      const productInCartIndex = state.findIndex(item => item.id === id)
-
-      if (productInCartIndex >= 0) {
-        const newState = structuredClone(state)
-        newState[productInCartIndex].quantity += 1
-        updateLocalStorage(newState)
-        return newState
-      }
-
-      const newState = [
-        ...state,
-        {
-          ...actionPayload,
-          quantity: 1
-        }
-      ]
-
+    if (productInCartIndex >= 0) {
+      const newState = structuredClone(state)
+      newState[productInCartIndex].quantity += 1
       updateLocalStorage(newState)
       return newState
     }
-    case 'REMOVE_FROM_CART': {
-      const { id } = actionPayload
-      return state.filter(item => item.id !== id)
-    }
-    case 'CLEAR_CART': {
-      updateLocalStorage([])
-      return []
-    }
+
+    const newState = [
+      ...state,
+      {
+        ...actionPayload,
+        quantity: 1
+      }
+    ]
+
+    updateLocalStorage(newState)
+    return newState
+  },
+  REMOVE_FROM_CART: (state, actionPayload) => {
+    const { id } = actionPayload
+    const newState = state.filter(item => item.id !== id)
+    updateLocalStorage(newState)
+    return newState
+  },
+  CLEAR_CART: () => {
+    updateLocalStorage([])
+    return []
+  }
+}
+
+export const reducer = (state, action) => {
+  const { type: actionType, payload: actionPayload } = action
+
+  if (actionType in actions) {
+    return actions[actionType](state, actionPayload)
   }
 
   return state
