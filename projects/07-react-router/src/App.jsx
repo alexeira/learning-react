de6a65e3efdd2521f94aaad3f0867c6768e83ce4
alexeira <alex.pincheira06@gmail.com>
2023-07-10 15:06:18 -0400

    clening App.jsx

--- projects/07-react-router/src/App.jsx
+++ projects/07-react-router/src/App.jsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from 'react'
+import { events } from './constants/events'
+import { HomePage } from './pages/Home'
+import { AboutPage } from './pages/About'
+
+const { pushstate, popstate } = events
+
 export default function App () {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname)
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname)
+    }
+
+    window.addEventListener(pushstate, onLocationChange)
+    window.addEventListener(popstate, onLocationChange)
+
+    return () => {
+      window.removeEventListener(pushstate, onLocationChange)
+      window.removeEventListener(popstate, onLocationChange)
+    }
+  }, [])
+
   return (
     <main>
-      <h1>Hola</h1>
+      <h1>React Router<code className='practice'>for practice</code></h1>
+      {currentPath === '/' && <HomePage />}
+      {currentPath === '/about' && <AboutPage />}
     </main>
   )
 }
