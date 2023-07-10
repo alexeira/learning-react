de405fea7532cf62cc64c2ee11e7d9d14b9bc5a7
alexeira <alex.pincheira06@gmail.com>
2023-07-10 15:05:55 -0400

    separating logic of navigate

---
+++ projects/07-react-router/src/utils/navigate.js
@@ -0,0 +1,10 @@
+import { events } from './constants/events'
+
+const { pushstate } = events
+
+export function navigate (href) {
+  window.history.pushState({}, '', href)
+  // crear un evento personalizado
+  const navigationEvent = new Event(pushstate)
+  window.dispatchEvent(navigationEvent)
+}
